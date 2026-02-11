6dbe0492bcde77822e5e284a7cce84d054d6b7d2
Diego17 <diegoalbertocastropastor@gmail.com>
2026-02-10 19:54:48 -0500

    Add playback session module with controller, entity, manager, service, types, and routes

---
+++ apps/backend/src/modules/playback-session/index.ts
@@ -0,0 +1,6 @@
+export * from './playbackSession.controller';
+export * from './playbackSession.entity';
+export * from './playbackSession.manager';
+export * from './playbackSession.service';
+export * from './playbackSession.types';
+export * from './playbackSessions.routes';
