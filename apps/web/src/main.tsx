fe7a2f0f19f394f1ef00a8a272318e4c53b41f5b
Diego17 <diegoalbertocastropastor@gmail.com>
2026-02-13 16:41:12 -0500

    Add React Query Devtools and integrate QueryClientProvider in main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./core/theme/index.css";
+import "@fontsource-variable/onest";
+import App from './app/App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+		<StrictMode>
+			<App />
+      <ReactQueryDevtools />
+		</StrictMode>
+	</QueryClientProvider>
 )
