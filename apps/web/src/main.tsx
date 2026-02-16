6b4aa6c41b5a98aa3cd6f8a785615df2357014b7
Diego17 <diegoalbertocastropastor@gmail.com>
2026-02-16 18:14:21 -0500

    Add HeroUI integration and update theme configuration

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./core/theme/index.css";
 import "@fontsource-variable/onest";
-import App from './app/App'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import App from "./app/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { HeroUIProvider } from "@heroui/system";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-		<StrictMode>
-			<App />
-      <ReactQueryDevtools />
-		</StrictMode>
-	</QueryClientProvider>
-)
+createRoot(document.getElementById("root")!).render(
+	<QueryClientProvider client={queryClient}>
+		<HeroUIProvider>
+			<StrictMode>
+				<App />
+				<ReactQueryDevtools />
+			</StrictMode>
+		</HeroUIProvider>
+	</QueryClientProvider>,
+);
