5c0c6d6b8e6de3a5059c897f69cd39e805acedbe
Diego17 <diegoalbertocastropastor@gmail.com>
2026-02-24 20:16:56 -0500

    Add constants for external links including GitHub repository and issues

---
+++ apps/web/src/core/constants/links.ts
@@ -0,0 +1,7 @@
+const GITHUB_BASE_URL = "https://github.com/Diego17cp"
+
+export const EXTERNAL_LINKS = {
+    github_repo: `${GITHUB_BASE_URL}/dialify`,
+    github_issues: `${GITHUB_BASE_URL}/dialify/issues`,
+    dev_email: "dialcadev@gmail.com"
+}
